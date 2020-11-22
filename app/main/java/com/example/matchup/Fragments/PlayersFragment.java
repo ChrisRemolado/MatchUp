7ab4ad9fb4c48c825c0fc453727186e10f85cf06
Chris Remolado <51104438+ChrisRemolado@users.noreply.github.com>
2020-11-21 20:10:27 -0500

    Add files via upload

---
+++ app/main/java/com/example/matchup/Fragments/PlayersFragment.java
@@ -0,0 +1,162 @@
+package com.example.matchup.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.matchup.Adapter.PlayersAdapter;
+import com.example.matchup.Model.User;
+import com.example.matchup.Model.User_Distance;
+import com.example.matchup.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+//Displays other registered players
+//TODO:
+// (a) Display registers players from closest distance to farthest distance
+// (b) onShortClick display user profile --> open another activity
+public class PlayersFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+
+    private RecyclerView recyclerView;
+    private PlayersAdapter playersAdapter;
+    private ArrayList<DataSnapshot> mUsers;
+    private SwipeRefreshLayout swipeRefreshLayout;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_players, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        swipeRefreshLayout = view.findViewById(R.id.swiperefresh);
+
+        PagerSnapHelper pagerSnapHelper = new PagerSnapHelper();
+        pagerSnapHelper.attachToRecyclerView(recyclerView);
+
+        mUsers = new ArrayList<>();
+
+        readUsers();
+
+        return view;
+    }
+
+    private void readUsers() {
+
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(getActivity()==null) return;
+                mUsers.clear();
+                double lat = 0.0, lon = 0.0;
+                ArrayList<String> user_sports = new ArrayList<>();
+                for(DataSnapshot snap: dataSnapshot.getChildren()){
+                    if(snap.getKey()!=firebaseUser.getUid())
+                        mUsers.add(snap);
+                    else {
+                        Object objLat = snap.child("location").child("lat").getValue();
+                        Object objLon = snap.child("location").child("lon").getValue();
+                        if(objLat != null && objLon != null){
+                            lat = (double) objLat;
+                            lon = (double) objLon;
+                        }
+                        // make arraylist of sports of the user
+                        for(DataSnapshot sport_name: snap.child("sports").getChildren()){
+                            user_sports.add(sport_name.getKey());
+                        }
+                    }
+                }
+                // Let's create an ArrayList of User_Distance objects
+                ArrayList<User_Distance> user_distances = new ArrayList<User_Distance>();
+                boolean key = false;
+                for(DataSnapshot snap: dataSnapshot.getChildren()){
+                    if(snap.getKey() != firebaseUser.getUid()){
+                        // check if common sports are available with the user, and only add to list if it is
+                        key = false;
+                        for(DataSnapshot sport_name: snap.child("sports").getChildren()){
+                            if(user_sports.contains(sport_name.getKey())){
+                                key = true;
+                            }
+                        }
+                        if(key == true){
+                            if (snap.child("location").child("lat").getValue() != null && snap.child("location").child("lon").getValue() != null) {
+                                User_Distance temp = new User_Distance();
+                                temp.setId(snap.getKey());
+                                temp.setDistance(distance(lat, lon, (double) snap.child("location").child("lat").getValue(), (double) snap.child("location").child("lon").getValue()));
+                                user_distances.add(temp);
+                            }
+                        }
+                    }
+                }
+                // Need to sort user_distances arraylist based on distance value
+                int n = user_distances.size();
+                for (int i = 0; i < n-1; i++) {
+                    int min_idx = i;
+                    for (int j = i+1; j < n; j++) {
+                        if (user_distances.get(j).getDistance() < user_distances.get(min_idx).getDistance()) {
+                            min_idx = j;
+                        }
+                    }
+                    /*
+                    User_Distance temp = user_distances.get(min_idx);
+                    user_distances.set(min_idx, user_distances.get(i));
+                    user_distances.set(i, temp);
+                    */
+                    Collections.swap(user_distances, min_idx, i);
+                }
+                // Then based on sorting, remake the mUsers array and apply to adapter to show
+                n = user_distances.size();
+                int n2 = mUsers.size();
+                for (int i = 0; i < n; i++) { // this will traverse through user distances
+                    for (int j = 0; j < n2; j++) { // this will traverse through mUsers
+                        if (user_distances.get(i).getId() == mUsers.get(j).getKey()){
+                            Collections.swap(mUsers, i, j);
+                            break;
+                        }
+                    }
+                }
+                playersAdapter = new PlayersAdapter(getContext(), mUsers, false, lat, lon);
+                recyclerView.setAdapter(playersAdapter);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
+    }
+
+    @Override
+    public void onRefresh() {
+        readUsers();
+    }
+
+    public double distance(double lat1, double lon1, double lat2, double lon2){
+        double theta = lon1 - lon2;
+        double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+        dist = Math.acos(dist);
+        dist = Math.toDegrees(dist);
+        dist = dist * 60 * 1.1515;
+        return dist;
+    }
+}
---
+++ app/main/java/com/example/matchup/MainActivity.java
@@ -0,0 +1,286 @@
+package com.example.matchup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.matchup.Fragments.ChatsFragment;
+import com.example.matchup.Fragments.PlayersFragment;
+import com.example.matchup.Fragments.ProfileFragment;
+import com.example.matchup.Model.User;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+//Main Activity
+public class MainActivity extends AppCompatActivity {
+
+    CircleImageView profile_image;
+    TextView username;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        profile_image = findViewById(R.id.profile_image);
+        username = findViewById(R.id.username);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(firebaseUser == null) return;
+                User user = dataSnapshot.getValue(User.class);
+
+                username.setText(user.getUsername());
+                if (user.getImageURL() == null || user.getImageURL().equals("default")) {
+                    profile_image.setImageResource(R.mipmap.ic_launcher);
+                } else {
+
+                    //Change this
+                    Glide.with(getApplicationContext()).load(user.getImageURL()).into(profile_image);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPagerAdapter.addFragment(new ChatsFragment(), "Chats");
+        viewPagerAdapter.addFragment(new PlayersFragment(), "Players");
+        viewPagerAdapter.addFragment(new ProfileFragment(), "Profile");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        if (checkLocationPermissions()) {
+            startLocationLogging();
+        } else {
+            // this will be done in the onRequestPermissionResult callback
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
+                finish();
+                return true;
+        }
+
+        return false;
+    }
+
+    private final int COARSE_LOCATION_REQUEST_PERMISSION = 98;
+
+    boolean checkLocationPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Location Permission")
+                        .setMessage("MatchUp requires your location data")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                        COARSE_LOCATION_REQUEST_PERMISSION);
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        COARSE_LOCATION_REQUEST_PERMISSION);
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case COARSE_LOCATION_REQUEST_PERMISSION: {
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    /**
+                     * TODO: make use of location permission
+                     */
+                    Toast.makeText(this, "Location granted", Toast.LENGTH_LONG)
+                            .show();
+                    startLocationLogging();
+                } else {
+                    // force app to close
+                    Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                    homeIntent.addCategory(Intent.CATEGORY_HOME);
+                    homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(homeIntent);
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    void startLocationLogging() {
+
+        ((LocationManager) getSystemService(Context.LOCATION_SERVICE))
+                .requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    0L,
+                    0f,
+                    new LocationListener() {
+
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            if(location==null) return;
+
+                            double lat = location.getLatitude();
+                            double lon = location.getLongitude();
+
+                            final DatabaseReference databaseLocation = reference.child("location");
+                            databaseLocation.child("lat").setValue(lat);
+                            databaseLocation.child("lon").setValue(lon);
+
+                            // DEBUG ONLY
+//                            Toast.makeText(MainActivity.this, "Location updated!", Toast.LENGTH_SHORT)
+//                                    .show();
+                        }
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) { }
+                        @Override
+                        public void onProviderEnabled(String provider) { }
+                        @Override
+                        public void onProviderDisabled(String provider) { }
+                    }
+                );
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        ViewPagerAdapter(FragmentManager fm){
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
+    }
+  
+    private void status(String status){
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("status", status);
+
+        reference.updateChildren(hashMap);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        status("online");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        status("offline");
+    }
+}
